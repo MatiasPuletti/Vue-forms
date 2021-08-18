406ff41695bfc2aa65c572a965ecc4d5285baa9c
matiaspuletti <pulettimatias@gmail.com>
2021-08-18 08:21:06 +0200

    Finish tweaking custom control component

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -99,7 +99,7 @@
       </div>
     </div>
     <div class="form-control">
-      <rating-control></rating-control>
+      <rating-control v-model="rating"></rating-control>
     </div>
     <div class="form-control">
       <input
@@ -131,7 +131,8 @@ export default {
       interest: [],
       how: null,
       confirm: false,
-      userNameValidity: 'pending'
+      userNameValidity: 'pending',
+      rating: null
     };
   },
   methods: {
@@ -153,6 +154,9 @@ export default {
       console.log('Confirm?');
       console.log(this.confirm);
       this.confirm = false;
+      console.log('Rating');
+      console.log(this.rating);
+      this.rating = null;
     },
     validateInput() {
       if (this.userName === '') {
